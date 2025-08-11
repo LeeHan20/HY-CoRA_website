f01e87f44ede69e9e42227a065cd508d23c4c716
이한 <hanlee@ihan-ui-MacBookAir.local>
2025-08-11 15:00:11 +0900

    public

--- public/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-<script>
-document.addEventListener('DOMContentLoaded', () => {
-  const path = location.pathname.replace(/\/+$/, '');
-  document.querySelectorAll('.nav a').forEach(a => {
-    const href = a.getAttribute('href');
-    if (path.endsWith(href)) a.classList.add('active');
-  });
-});
-</script>
